a4233c44240eaa820f762e7ed966c621cda9e65a
Adimgr1 <adimgr51@gmail.com>
2023-12-06 15:48:39 +0530

    day5

--- app/src/main/java/com/aditya/myapplication/HomeFragment.kt
+++ app/src/main/java/com/aditya/myapplication/HomeFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class HomeFragment : Fragment() {
     override fun onCreateView(
@@ -17,12 +19,15 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         var exampleList = listOf<infomembers>(
-            infomembers("Aditya Kumar", 8955163181),
+            infomembers("Ajay", 8955163181),
             infomembers("Rounak Vats", 8655143210),
             infomembers("Rohit Raj", 8655143210),
-            infomembers("Rakesh Gautam", 8655143210),
+            infomembers("Rakesh Gautam", 8655143210)
         )
         val homeAdapter1 = memberadapter(exampleList)
+        val recycler = requireView().findViewById<RecyclerView>(R.id.recyclerOne)
+        recycler.layoutManager= LinearLayoutManager(requireContext())
+        recycler.adapter= homeAdapter1
 
 
 
--- app/src/main/java/com/aditya/myapplication/memberadapter.kt
+++ app/src/main/java/com/aditya/myapplication/memberadapter.kt
@@ -1,24 +1,34 @@
 package com.aditya.myapplication
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class memberadapter(exampleList: List<infomembers>) : RecyclerView.Adapter<memberadapter.ViewHolder>() {
-    class ViewHolder(val a : View) : RecyclerView.ViewHolder(a) {
+class memberadapter(private val exampleList: List<infomembers>) : RecyclerView.Adapter<memberadapter.ViewHolder>() {
 
-
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): memberadapter.ViewHolder {
-        TODO("Not yet implemented")
+        val inflater = LayoutInflater.from(parent.context)
+        val item = inflater.inflate(R.layout.memberscard,parent,false)
+        return ViewHolder(item)
     }
 
     override fun onBindViewHolder(holder: memberadapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val item = exampleList[position]
+        holder.name.text = item.name
+        holder.number.text= item.number.toString()
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return exampleList.size
+    }
+    class ViewHolder(private val a : View) : RecyclerView.ViewHolder(a) {
+        val name = a.findViewById<TextView>(R.id.Name)
+        val number = a.findViewById<TextView>(R.id.number)
+
+
+
     }
 }
